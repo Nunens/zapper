608051c3b3a9fe275c31b13db555e92c9aba5d59
S Kobue <siphokobue@gmail.com>
2022-08-18 13:15:45 +0200

    Missed files

--- app/src/main/java/za/co/sikabopha/App.java
+++ app/src/main/java/za/co/sikabopha/App.java
@@ -1,7 +1,9 @@
 package za.co.sikabopha;
 
+import android.app.Application;
+
 import dagger.hilt.android.HiltAndroidApp;
 
 @HiltAndroidApp
-public class App {
+public class App extends Application {
 }
